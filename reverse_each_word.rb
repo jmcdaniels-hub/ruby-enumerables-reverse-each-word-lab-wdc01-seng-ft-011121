aabfe4d85b4910f06541377007359ab6a5db4852
Jade McDaniels <jademcdaniels81@gmail.com>
2021-01-03 19:34:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence1)
-  sentence_array = sentence.split(" ")
+  
+  sentence_array = sentence1.split(" ")
 
 
   (sentence_array.collect {|element|element.reverse}).join(" ")
 
- 
   
 end 
